d425eaafc6dfceb0fb4d659e1f103e0e7ff879d1
Divyank Singh <158442628+divyank369@users.noreply.github.com>
2025-07-10 22:34:24 +0530

    Delete themeToggle.js

--- themeToggle.js
+++
@@ -1,8 +0,0 @@
-const themeToggleBtn = document.getElementById("themeToggle");
-
-themeToggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("light");
-  document.body.classList.toggle("dark");
-
-  themeToggleBtn.textContent = document.body.classList.contains("light") ? "🌞" : "🌙";
-});
